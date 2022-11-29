a078306378bd5e9c541f9897a69a45f01fb2139e
Romack Natividad <romacknatividad@gmail.com>
2022-11-29 23:42:11 +0800

    Updated connection

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,14 @@
-  <?php
+<?php
 
-define("server","localhost");
-define("username","root");
-define("password","");
-define("dbname","barangay");
+require("./../vendor/autoload.php");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+define("server", $_ENV['DB_HOST']);
+define("username", $_ENV['DB_USER']);
+define("password", $_ENV['DB_PASS']);
+define("dbname", $_ENV['DB_NAME']);
 
 // Create connection
 $dbconnection = mysqli_connect(server, username, password,dbname);
